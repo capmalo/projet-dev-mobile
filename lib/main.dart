39650cf4d1273df8445ce131d9c21b56a2624d94
Theodav <theodore.d.avray@efrei.net>
2019-12-02 15:57:27 +0100

    Nouveau menu de navigation

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,24 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Projet de dev mobile"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0, // this will be set when a new tab is tapped
+        items: [
+          BottomNavigationBarItem(
+              icon: new Icon(Icons.assignment),
+              title: new Text('Notes'),
+          ),
+          BottomNavigationBarItem(
+              icon: new Icon(Icons.person_pin),
+              title: new Text('Utilisateurs'),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.info_outline),
+              title: Text('A propos')
+          )
+        ],
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
