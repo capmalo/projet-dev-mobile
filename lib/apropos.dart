23bc2569863949a22d3d7c386e2072a294994d9a
capmalo <malofernandez29@hotmail.fr>
2019-12-02 18:06:44 +0100

    Page à propos finie (juste à remplir)

--- lib/apropos.dart
+++ lib/apropos.dart
@@ -1,20 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/main.dart';
 
 class aProposPage extends StatelessWidget {
 
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: RaisedButton(
-              child: Text('Go to Second Screen'),
-              color: Theme.of(context).primaryColor,
-              textColor: Colors.white,
-              onPressed: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (BuildContext context) => MyHomePage(),
-                  )))),
+          child: ListView(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(left: 70,right: 70,top: 10),
+                child: Image(image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/e/e7/Logo-Efrei-Paris-2017.jpg'),),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: Text('This magnificent app is truthfully presented to you by :',
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                  //style: TextStyle(fontWeight: FontWeight.bold)
+                ),
+              ),
+              Text('Malo FERNANDEZ',
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontWeight: FontWeight.bold)
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: Text('and',
+                    textAlign: TextAlign.center,
+                    overflow: TextOverflow.ellipsis,
+                    //style: TextStyle(fontWeight: FontWeight.bold)
+                ),
+              ),
+              Text("Théodore D'AVRAY",
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontWeight: FontWeight.bold)
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10,top: 50),
+                child: Text.rich(
+                  TextSpan(
+                    text: 'Les difficultées rencontrées :', style: TextStyle(fontWeight: FontWeight.bold),
+                    children: <TextSpan>[
+                      TextSpan(text: '\n- Difficulté 1', style: TextStyle(fontWeight: FontWeight.normal)),
+                      TextSpan(text: '\n- Difficulté 2', style: TextStyle(fontWeight: FontWeight.normal)),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10,top: 30),
+                child: Text.rich(
+                  TextSpan(
+                    text: 'Les bibliothèques tierces utilisées :', style: TextStyle(fontWeight: FontWeight.bold),
+                    children: <TextSpan>[
+                      TextSpan(text: '\n- Bibliothèque 1', style: TextStyle(fontWeight: FontWeight.normal)),
+                      TextSpan(text: '\n- Bibliothèque 2', style: TextStyle(fontWeight: FontWeight.normal)),
+                    ],
+                  ),
+                 ),
+              ),
+            ],
+          )
+      )
     );
   }
 }
