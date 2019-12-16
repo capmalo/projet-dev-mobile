8b79625d5694eab4ac12534368c9f79405dac7fb
capmalo <malofernandez29@hotmail.fr>
2019-12-16 16:16:10 +0100

    List users design préparé

--- lib/users/listusers.dart
+++ lib/users/listusers.dart
@@ -53,16 +53,68 @@ class _ListUsersScreenState extends State {
   }
 
   @override
-  build(context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("User List"),
-        ),
-        body: ListView.builder(
-          itemCount: users.length,
-          itemBuilder: (context, index) {
-            return ListTile(title: Text(users[index].name));
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Random Users"),
+        centerTitle: false,
+      ),
+      body: new Container(
+        child: new FutureBuilder(
+          //future: userDetails(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (users == null) {
+              return new Center(
+                child: new CircularProgressIndicator(),
+              );
+            } else {
+              return ListView.builder(
+                itemCount: users.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return new ListTile(
+                    /*leading: new Hero(
+                      /*tag: "image$index",
+                      child: CircleAvatar(
+                        backgroundImage:
+                        new NetworkImage(snapshot.data[index].pictureSmall),
+                      ),*/
+                    ),*/
+                    title: new Text(
+                      users[index].name +
+                          " " +
+                          users[index].name,
+                      style: new TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    //subtitle: new Text(snapshot.data[index].email),
+                   /* trailing: new SizedBox(
+                      height: 20.0,
+                      child: new Image.asset(menOrWomen(snapshot.data[index].gender)),
+                    ),*/
+                    /*onTap: () {
+                      Navigator.push(
+                        context,
+                        new MaterialPageRoute(
+                          builder: (context) =>
+                              Details(snapshot.data[index], index),
+                        ),
+                      );
+                    },*/
+                  );
+                },
+              );
+            }
           },
-        ));
+        ),
+      ),
+     /* floatingActionButton: new FloatingActionButton(
+        tooltip: "Reload",
+        child: new Icon(Icons.refresh),
+        onPressed: () {
+          _reload();
+        },
+      ),*/
+    );
   }
 }
